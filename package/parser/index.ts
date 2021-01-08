847aa5b4c10d8374042b29c979caa4f2335ffd1b
Stormy <russellsorin2@gmail.com>
2021-01-08 14:16:40 -0600

    code: display

--- package/parser/index.ts
+++ package/parser/index.ts
@@ -1,8 +1,8 @@
 import { workshopScript } from "./language/global";
 
 /* const workshopScript */
-const getData = (f: string) => workshopScript.tryParse(f);
+const parse = (f: string) => workshopScript.tryParse(f);
 
 export {
-  getData,
+  parse,
 };
